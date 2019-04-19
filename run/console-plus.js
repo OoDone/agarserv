69f63f8f7572e8d92b66e9790679d0d5c4634e43
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-04-19 11:59:06 -0400

    Update console-plus.js

--- run/console-plus.js
+++ run/console-plus.js
@@ -176,7 +176,7 @@ const config = require('./console-plus.json');
         perMessageDeflate: false,
         maxPayload: 4096,
         protocolVersion: 8,
-        origin:'http://' + config.ip + ':' + process.env.PORT, || 8080
+        origin:'http://' + config.ip + ':' + process.env.PORT || 8080,
         autoAcceptConnections: false
     };
 
