55f4e23b1d680c93abea38c58bb987658fea4533
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-04-22 14:33:45 -0400

    Update console-plus.js

--- run/console-plus.js
+++ run/console-plus.js
@@ -138,7 +138,7 @@ const config = require('./console-plus.json');
         });
       }
 
-  }).listen(process.env.PORT || 8080, function() {
+  }).listen(process.env.PORT, function() {
         console.log(/*(new Date()) + */' consoleServer is listening on port ' + config.port);
      });
 
@@ -176,7 +176,7 @@ const config = require('./console-plus.json');
         perMessageDeflate: false,
         maxPayload: 4096,
         protocolVersion: 8,
-        origin:'http://' + config.ip + ':' + process.env.PORT || 8080,
+        origin:'http://' + config.ip + ':' + config.port,
         autoAcceptConnections: false
     };
 
