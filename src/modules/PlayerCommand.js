bff1fd2e17eb7ef0afae548718175f179c262bdd
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-04-24 13:11:30 -0400

    Update PlayerCommand.js

--- src/modules/PlayerCommand.js
+++ src/modules/PlayerCommand.js
@@ -87,6 +87,72 @@ var playerCommands = {
     },
 
 
+    playerlist: function (gameServer, split) {
+        if (!gameServer.clients.length) return this.writeLine("No bots or players are currently connected to the server!");
+        this.writeLine("\nCurrent players: " + gameServer.clients.length);
+        this.writeLine('Do "playerlist m" or "pl m" to list minions\n');
+        this.writeLine(" ID     | IP              | P | CELLS | SCORE  |   POSITION   | " + fillChar('NICK', ' ', gameServer.config.playerMaxNickLength) + " "); // Fill space
+        this.writeLine(fillChar('', '─', ' ID     | IP              | CELLS | SCORE  |   POSITION   |   |  '.length + gameServer.config.playerMaxNickLength));
+        var sockets = gameServer.clients.slice(0);
+        sockets.sort(function (a, b) {
+            return a.playerTracker.pID - b.playerTracker.pID;
+        });
+        for (var i = 0; i < sockets.length; i++) {
+            var socket = sockets[i];
+            var client = socket.playerTracker;
+            var type = split[1];
+
+            // ID with 3 digits length
+            var id = fillChar((client.pID), ' ', 6, true);
+
+            // Get ip (15 digits length)
+            var ip = client.isMi ? "[MINION]" : "[BOT]";
+            if (socket.isConnected && !client.isMi) {
+                ip = socket.remoteAddress;
+            } else if (client.isMi && type != "m") {
+                continue; // do not list minions
+            }
+            ip = fillChar(ip, ' ', 15);
+
+            // Get name and data
+            var protocol = gameServer.clients[i].packetHandler.protocol;
+            if (!protocol) protocol = "?";
+            var nick = '',
+                cells = '',
+                score = '',
+                position = '',
+                data = '';
+            if (socket.closeReason != null) {
+                // Disconnected
+                var reason = "[DISCONNECTED] ";
+                if (socket.closeReason.code)
+                    reason += "[" + socket.closeReason.code + "] ";
+                if (socket.closeReason.message)
+                    reason += socket.closeReason.message;
+                this.writeLine(" " + id + " | " + ip + " | " + protocol + " | " + reason);
+            } else if (!socket.packetHandler.protocol && socket.isConnected && !client.isMi) {
+                this.writeLine(" " + id + " | " + ip + " | " + protocol + " | " + "[CONNECTING]");
+            } else if (client.spectate) {
+                nick = "in free-roam";
+                if (!client.freeRoam) {
+                    var target = client.getSpecTarget();
+                    if (target) nick = getName(target._name);
+                }
+                data = fillChar("SPECTATING: " + nick, '-', ' | CELLS | SCORE  | POSITION    '.length + gameServer.config.playerMaxNickLength, true);
+                this.writeLine(" " + id + " | " + ip + " | " + protocol + " | " + data);
+            } else if (client.cells.length) {
+                nick = fillChar(getName(client._name), ' ', gameServer.config.playerMaxNickLength);
+                cells = fillChar(client.cells.length, ' ', 5, true);
+                score = fillChar(getScore(client) >> 0, ' ', 6, true);
+                position = fillChar(getPos(client).x >> 0, ' ', 5, true) + ', ' + fillChar(getPos(client).y >> 0, ' ', 5, true);
+                this.writeLine(" " + id + " | " + ip + " | " + protocol + " | " + cells + " | " + score + " | " + position + " | " + nick);
+            } else {
+                // No cells = dead player or in-menu
+                data = fillChar('DEAD OR NOT PLAYING', '-', ' | CELLS | SCORE  | POSITION    '.length + gameServer.config.playerMaxNickLength, true);
+                this.writeLine(" " + id + " | " + ip + " | " + protocol + " | " + data);
+            }
+        }
+    },
     id: function (args) {
         this.writeLine("Your PlayerID is " + this.playerTracker.pID);
     },
