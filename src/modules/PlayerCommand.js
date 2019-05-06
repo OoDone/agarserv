710bbcb63b16ce06871b429b882aca08e79670ce
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-06 10:55:01 -0400

    Update PlayerCommand.js

--- src/modules/PlayerCommand.js
+++ src/modules/PlayerCommand.js
@@ -6,6 +6,18 @@ function PlayerCommand(gameServer, playerTracker) {
     this.playerTracker = playerTracker;
 }
 
+var fillChar = function (data, char, fieldLength, rTL) {
+    var result = data.toString();
+    if (rTL === true) {
+        for (var i = result.length; i < fieldLength; i++)
+            result = char.concat(result);
+    } else {
+        for (var i = result.length; i < fieldLength; i++)
+            result = result.concat(char);
+    }
+    return result;
+};
+
 module.exports = PlayerCommand;
 
 PlayerCommand.prototype.writeLine = function (text) {
