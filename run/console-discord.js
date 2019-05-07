b4596de2e4b905319ffc96d757370a43dd2acca3
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-07 16:06:33 -0400

    Update console-discord.js

--- run/console-discord.js
+++ run/console-discord.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const Commands = require("../src/modules/CommandList");
 const index = require("../src/index");
 const Logger = require("../src/modules/Logger");
-const logs = require("../src/logs/serverlog.log");
+const logs = require("../src/logs/serverlog");
 const config = {
     role: "owner",
     token: "NTY4Nzc1OTU3NDkwNjk2MTky.XLm_8g.QABmXoQkLG1_YZE3WPVudVwsejk"
