ff84e863d430fb0816198e8116fadf6db3c6d380
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-08 12:58:59 -0400

    Update console-discord.js

--- run/console-discord.js
+++ run/console-discord.js
@@ -25,7 +25,7 @@ class Bot {
         if (typeof execute != 'undefined' && message.member.roles.some(r => [config.role].includes(r.name))) {
             execute(index.gameServer, args);
             message.delete();
-            if (message.content = "s");{
+            if (message.content.includes("s"));{
                 message.channel.send("test");
             }
         } else {
