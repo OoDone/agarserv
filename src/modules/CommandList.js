22a632c33f86b90ddb0b06b01994e003099c3acb
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-09 10:55:06 -0400

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -635,8 +635,8 @@ Commands.list = {
             }
         }
         if (client == null) return void Logger.warn("That player ID is non-existant!");
-        Logger.info("Set base speed of " + getName(client._name) + " to " + speed);
-        client.on('message', function(message) {
+        global.speed2 = "blah blah" + getName(client._name) + " to " + speed;
+        client2.on('message', function(message) {
             message.channel.send("Set base speed of " + getName(client._name) + " to " + speed);
         });
     },
