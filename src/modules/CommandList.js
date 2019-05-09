78087b1b7dbdffbe7807dfe499012ad961036685
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-09 13:09:12 -0400

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -72,7 +72,7 @@ Commands.list = {
             "│ unban [IP]                   │ Unbans an IP                              │\n" +
             "│ banlist                      │ Get list of banned IPs.                   │\n" +
             "│ mute [PlayerID]              │ Mute player from chat by client ID        │```"
-        global.help22 = "```│ unmute [PlayerID]            │ Unmute player from chat by client ID       │\n" +
+        global.help22 = "```│ unmute [PlayerID]            │ Unmute player from chat by client ID      │\n" +
             "| lms                          | Starts/ends last man standing             |\n" +
             "| chat                         | Sends a server message to all clients     |\n" +
             "│                                                                          │\n" +
