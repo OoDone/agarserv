3c4fc74d52bf33c423e6aeb066646fb5ec99cd4b
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-08 20:25:24 -0400

    Update console-discord.js

--- run/console-discord.js
+++ run/console-discord.js
@@ -34,70 +34,7 @@ class Bot {
             }
             if (message.content.startsWith("pl")) {
                 if (message.content == 'pl') {
-                    if (!gameServer.clients.length) return message.channel.send("No bots or players are currently connected to the server!");
-        message.channel.send("\nCurrent players: " + gameServer.clients.length);
-        message.channel.send('Do "playerlist m" or "pl m" to list minions\n');
-        message.channel.send(" ID     | IP              | P | CELLS | SCORE  |   POSITION   | " + fillChar('NICK', ' ', gameServer.config.playerMaxNickLength) + " "); // Fill space
-        Logger.print(fillChar('', '─', ' ID     | IP              | CELLS | SCORE  |   POSITION   |   |  '.length + gameServer.config.playerMaxNickLength));
-        var sockets = gameServer.clients.slice(0);
-        sockets.sort(function (a, b) {
-            return a.playerTracker.pID - b.playerTracker.pID;
-        });
-        for (var i = 0; i < sockets.length; i++) {
-            var socket = sockets[i];
-            var client = socket.playerTracker;
-            var type = split[1];
-
-            // ID with 3 digits length
-            var id = fillChar((client.pID), ' ', 6, true);
-
-            // Get ip (15 digits length)
-            var ip = client.isMi ? "[MINION]" : "[BOT]";
-            if (socket.isConnected && !client.isMi) {
-                ip = socket.remoteAddress;
-            } else if (client.isMi && type != "m") {
-                continue; // do not list minions
-            }
-            ip = fillChar(ip, ' ', 15);
-
-            // Get name and data
-            var protocol = gameServer.clients[i].packetHandler.protocol;
-            if (!protocol) protocol = "?";
-            var nick = '',
-                cells = '',
-                score = '',
-                position = '',
-                data = '';
-            if (socket.closeReason != null) {
-                // Disconnected
-                var reason = "[DISCONNECTED] ";
-                if (socket.closeReason.code)
-                    reason += "[" + socket.closeReason.code + "] ";
-                if (socket.closeReason.message)
-                    reason += socket.closeReason.message;
-                message.channel.send(" " + id + " | " + ip + " | " + protocol + " | " + reason);
-            } else if (!socket.packetHandler.protocol && socket.isConnected && !client.isMi) {
-                message.channel.send(" " + id + " | " + ip + " | " + protocol + " | " + "[CONNECTING]");
-            } else if (client.spectate) {
-                nick = "in free-roam";
-                if (!client.freeRoam) {
-                    var target = client.getSpecTarget();
-                    if (target) nick = getName(target._name);
-                }
-                data = fillChar("SPECTATING: " + nick, '-', ' | CELLS | SCORE  | POSITION    '.length + gameServer.config.playerMaxNickLength, true);
-                message.channel.send(" " + id + " | " + ip + " | " + protocol + " | " + data);
-            } else if (client.cells.length) {
-                nick = fillChar(getName(client._name), ' ', gameServer.config.playerMaxNickLength);
-                cells = fillChar(client.cells.length, ' ', 5, true);
-                score = fillChar(getScore(client) >> 0, ' ', 6, true);
-                position = fillChar(getPos(client).x >> 0, ' ', 5, true) + ', ' + fillChar(getPos(client).y >> 0, ' ', 5, true);
-                message.channel.send(" " + id + " | " + ip + " | " + protocol + " | " + cells + " | " + score + " | " + position + " | " + nick);
-            } else {
-                // No cells = dead player or in-menu
-                data = fillChar('DEAD OR NOT PLAYING', '-', ' | CELLS | SCORE  | POSITION    '.length + gameServer.config.playerMaxNickLength, true);
-                message.channel.send(" " + id + " | " + ip + " | " + protocol + " | " + data);
-            }
-        }
+                   message.channel.send("Playerlist Discord In Progress");
                }
             }
         } else {
