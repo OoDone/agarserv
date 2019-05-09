567a3716c3b640c62fc9e01cb6dcc5733a3912c1
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-09 13:06:23 -0400

    Update CommandList.js

--- src/modules/CommandList.js
+++ src/modules/CommandList.js
@@ -137,18 +137,22 @@ Commands.list = {
         var ent = split[1];
         if (ent != "ejected" && ent != "food" && ent != "virus") {
             global.reset1 = "Removed " + gameServer.nodes.length + " nodes";
+            global.reset1t = true;
             for (; gameServer.nodes.length;) gameServer.removeNode(gameServer.nodes[0]);
         }
         if (ent == "ejected") {
             global.reset2 = "Removed " + gameServer.nodesEjected.length + " ejected nodes";
+            global.reset2t = true;
             for (; gameServer.nodesEjected.length;) gameServer.removeNode(gameServer.nodesEjected[0]);
         }
         if (ent == "food") {
             global.reset3 = "Removed " + gameServer.nodesFood.length + " food nodes";
+            global.reset3t = true;
             for (; gameServer.nodesFood.length;) gameServer.removeNode(gameServer.nodesFood[0]);
         }
         if (ent == "virus") {
             global.reset4 = "Removed " + gameServer.nodesVirus.length + " virus nodes";
+            global.reset4t = true;
             for (; gameServer.nodesVirus.length;) gameServer.removeNode(gameServer.nodesVirus[0]);
         }
     },
