fbd21aeb401244a425622c452d0ab968837d2758
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-10 12:07:57 -0400

    Update console-discord.js

--- run/console-discord.js
+++ run/console-discord.js
@@ -130,10 +130,11 @@ class Bot {
                 };
                   function playerlist(gameServer, split) {
                         if (!gameServer.clients.length) message.channel.send("```No bots or players are currently connected to the server!```");
-                            message.channel.send("```\nCurrent players: " + gameServer.clients.length + "\n"
+                            global.playerlist2 = "```\nCurrent players: " + gameServer.clients.length + "\n" +
                             'Do "playerlist m" or "pl m" to list minions\n' + 
-                            " ID     | IP              | P | CELLS | SCORE  |   POSITION   | " + fillChar('NICK', ' ', gameServer.config.playerMaxNickLength) + " \n" +// Fill space
+                            " ID     | IP              | P | CELLS | SCORE  |   POSITION   | " + fillChar('NICK', ' ', gameServer.config.playerMaxNickLength) + " \n" +
                             fillChar('', '─', ' ID     | IP              | CELLS | SCORE  |   POSITION   |   |  '.length + gameServer.config.playerMaxNickLength) + "```";
+                            message.channel.send(playerlist2);
                             global.sockets = gameServer.clients.slice(0);
                             sockets.sort(function (a, b) {
                                 return a.playerTracker.pID - b.playerTracker.pID;
