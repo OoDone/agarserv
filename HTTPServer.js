e6fc31297e2c08376248e8d1a1c786a261e79b8c
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-10-18 13:30:04 -0400

    Update HTTPServer.js

--- HTTPServer.js
+++ HTTPServer.js
@@ -2,11 +2,12 @@ const http = require('http')
 
 const hostname = '0.0.0.0'
 const port = 5000 && process.env.PORT
+var xd = require('./console.html');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html');
-  res.write(require('./console.html'));
+  res.write(xd);
   res.end();
 })
 
