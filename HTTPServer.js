95d92f021e16c9b6d86cf88c240e304b6901fb3d
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-10-18 13:38:45 -0400

    Update HTTPServer.js

--- HTTPServer.js
+++ HTTPServer.js
@@ -21,8 +21,7 @@ const server = http.createServer((req, res) => {
   res.write(html);
   res.end();
    })
-})
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+  server.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`)
+  })
 })
