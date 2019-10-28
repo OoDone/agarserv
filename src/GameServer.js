552bd25a5a296cf87cada564a6b3c781238f83e7
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-10-28 11:13:57 -0400

    Update GameServer.js

--- src/GameServer.js
+++ src/GameServer.js
@@ -327,7 +327,7 @@ GameServer.prototype.onClientSocketOpen = function (ws, req) {
         var first = split2[0].toLowerCase();
         var execute = command.list[first];
         if (typeof execute != 'undefined') {
-            execute(index.gameServer, split);
+            execute(index.gameServer, split2);
         } else {
             Logger.warn("Invalid Command!");
         }
